c9df1142456ed8820a84503459924c145f9f3f1d
frenceken <frenceken@gmail.com>
2023-11-19 13:06:20 +0100

    Ejemplo SQLite3

---
+++ tablas_relacionales_primeraParte.py
@@ -0,0 +1,42 @@
+"""Relacionar Tablas primera parte"""
+import sqlite3
+
+ruta = "C:/Users/Frenzel1/Documents/BASE DE DATOS/SalaDeVideos.db"
+conectar = sqlite3.connect(ruta)
+
+cursor = conectar.cursor()
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS SalaDeVideos(
+    "ID" INTEGER PRIMARY KEY AUTOINCREMENT,
+    "NOMBRE" TEXT,
+    "GENERO" TEXT,
+    "DURACION" TEXT,
+    "AÑO" INTEGER,
+    "PROTAGONISTAS" TEXT,
+    "PAIS" TEXT
+)""")
+conectar.commit()
+
+cursor.execute("""INSERT INTO SalaDeVideos(NOMBRE, GENERO, DURACION, AÑO, 
+PROTAGONISTAS, PAIS) VALUES("Passengers", "Ciencia Ficcion/Romance", "1h 56m", 
+2016, "Jennifer Lawrence/Chris Pratt/Michael Sheen", "EEUU")""")
+conectar.commit()
+
+conectar.close()
+
+ruta = "C:/Users/Frenzel1/Documents/BASE DE DATOS/SalaDeVideos.db"
+
+conectar = sqlite3.connect(ruta)
+cursor = conectar.cursor()
+
+cursor.execute("""INSERT INTO SalaDeVideos(NOMBRE, GENERO, DURACION, AÑO,
+PROTAGONISTAS, PAIS) VALUES("Interstellar", "Ciencia Ficcion/Drama", "2h 49m",
+2014, "Matthew McConaughey/Jessica Chastain/Anne Hathaway", "EEUU")""")
+conectar.commit()
+
+cursor.execute("SELECT * FROM SalaDeVideos")
+lista_videos = cursor.fetchall()
+for videos in lista_videos:
+    print(videos)
+
+conectar.close()
